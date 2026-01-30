8fe5f2367c13a32d58d702207370621d34e48ad1
05stefanovb <05stefanovb@gmail.com>
2026-01-30 23:48:06 +0000

    Add files via upload

---
+++ portfolio optimisation.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 23:14:50 2026
+
+@author: slopingcorn
+"""
+from datetime import datetime, timedelta
+import numpy as np
+import yfinance as yf
+import pandas as pd
+from scipy.optimize import minimize
+
+#define tickers of companies to be optimized
+tickers = [
+    'AAPL', 'MSFT', 'NVDA', 'AMZN', 'GOOGL', 'META', 'BRK-B', 'GOOG', 'TSLA',
+    'LLY', 'AVGO', 'JPM', 'UNH', 'XOM', 'V', 'MA', 'HD', 'PG', 'COST', 'MRK',
+    'ABBV', 'CVX', 'PEP', 'ADBE', 'KO', 'CRM', 'NFLX', 'AMD', 'TMO', 'WMT',
+    'DIS', 'MCD', 'INTC', 'ACN', 'QCOM', 'CSCO', 'TXN', 'NEE', 'LIN', 'PM',
+    'HON', 'AMGN', 'IBM', 'UPS', 'MS', 'GS', 'SBUX', 'CAT', 'BA']
+#define start and end date (years is number of years before end date (255 trading days))
+years = 5
+end_date = datetime.today()
+start_date = end_date - timedelta(days = years*255)
+
+
+adj_close_df = pd.DataFrame()
+for ticker in tickers:
+    data = yf.download(ticker, start = start_date,end = end_date)
+    adj_close_df[ticker] = data['Close']
+
+log_returns = np.log(adj_close_df / adj_close_df.shift(1)).dropna()
+    
+cov_matrix = log_returns.cov() * 252
+
+def standard_deviation(weights, cov_matrix):
+    variance = weights.T @ cov_matrix @ weights
+    return np.sqrt(variance)
+
+def expected_return(weights, log_returns):
+    return np.sum(log_returns.mean()*weights)*252
+
+def sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate):
+    return (expected_return(weights, log_returns) - risk_free_rate) / standard_deviation(weights, cov_matrix)
+
+risk_free_rate = .02
+
+def neg_sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate):
+    return -sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate)
+
+constraints = {'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}
+bounds = [(0, 0.4) for _ in range(len(tickers))]
+initial_weights = np.array([1/len(tickers)]*len(tickers))
+
+optimized_results = minimize(neg_sharpe_ratio, initial_weights, args=(log_returns, cov_matrix, risk_free_rate), method='SLSQP', constraints=constraints, bounds=bounds)
+
+optimal_weights = optimized_results.x
+
+weights = pd.Series(optimized_results.x, index=tickers)
+
+threshold = 1e-2
+weights = weights[weights.abs() > threshold]
+
+weights /= weights.sum()
+
+print("Optimal Weights:")
+for ticker, weight in weights.items():
+    print(f"{ticker}: {weight:.4f}")
+    
+    
+optimal_portfolio_return = expected_return(optimal_weights, log_returns)
+optimal_portfolio_volatility = standard_deviation(optimal_weights, cov_matrix)
+optimal_sharpe_ratio = sharpe_ratio(optimal_weights, log_returns, cov_matrix, risk_free_rate)
+
+print(f"Expected Annual Return: {optimal_portfolio_return:.4f}")
+print(f"Expected Volatility: {optimal_portfolio_volatility:.4f}")
+print(f"Sharpe Ratio: {optimal_sharpe_ratio:.4f}")
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+plt.bar(weights.index, weights.values)
+plt.xticks(rotation=45)
+
+plt.xlabel('Assets')
+plt.ylabel('Optimal Weights')
+plt.title('Optimal Portfolio Weights')
+
+plt.show()
