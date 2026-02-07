f6106ce535937bb2677be55051c5909d7c1f3595
05stefanovb <05stefanovb@gmail.com>
2026-02-07 21:08:29 +0000

    portfolio optimisation ridge iteration 1

--- portfolio optimisation ridge regression.py
+++ portfolio optimisation ridge regression.py
@@ -15,12 +15,12 @@ tickers = [
     'LLY', 'AVGO', 'JPM', 'UNH', 'XOM', 'V', 'MA', 'HD', 'PG', 'COST', 'MRK',
     'ABBV', 'CVX', 'PEP', 'ADBE', 'KO', 'CRM', 'NFLX', 'AMD', 'TMO', 'WMT',
     'DIS', 'MCD', 'INTC', 'ACN', 'QCOM', 'CSCO', 'TXN', 'NEE', 'LIN', 'PM',
-    'HON', 'AMGN', 'IBM', 'UPS', 'MS', 'GS', 'SBUX', 'CAT', 'BA'
+    'HON', 'AMGN', 'IBM', 'UPS', 'MS', 'GS', 'SBUX', 'CAT', 'BA', "CVNA"
 ]
 
 
-years = 2
-end_date = datetime.today() - relativedelta(years = 10)   # your original choice
+years = 5
+end_date = datetime.today() - relativedelta(years = 0)   # your original choice
 start_date = end_date - relativedelta(years=years)
 
 
@@ -29,26 +29,11 @@ for ticker in tickers:
     data = yf.download(ticker, start=start_date, end=end_date, progress=False)
     adj_close[ticker] = data['Close']
 
-
+#%%
 
 log_returns = np.log(adj_close / adj_close.shift(1)).dropna()
 mu = log_returns.mean().values * 252
 cov_matrix = log_returns.cov().values * 252
-
-
-
-#define application of constraints (minimum and maximum weights, sum to 1)
-def apply_constraints(w, cap=0.4):
-    """
-    Simple projection used in your code: clip to [0, cap] then renormalize (exclude weights less than threshold and redistributes proportionally).
-    """
-    # (w must lie between 0 and cap)
-    w = np.clip(w, 0.0, cap)
-    
-    #redistribute weights proportionally so it sums to 1
-    if w.sum() > 0:
-        w /= w.sum()
-    return w
 
 def expected_return(w, log_returns):
     return np.sum(log_returns.mean().values * w) * 252
@@ -59,85 +44,162 @@ def standard_deviation(w, cov_matrix):
 def sharpe_ratio(w, log_returns, cov_matrix, rfr):
     return (expected_return(w, log_returns) - rfr) / standard_deviation(w, cov_matrix)
 
+def projection(v, total=1.0, cap=0.4, tol=1e-12, max_iter=2000):
+    """
+    A projection which takes an input of weights (v), shifts it by a certain scalar tau to make it sum to "total" and applies certain constraints to project in desired range 
+
+    Parameters
+    ----------
+    v : array
+        vector of input weights (the weights we are projecting)
+    total : TYPE, optional
+        what the sum of the weights should equal. The default is 1.0.
+    cap : float, optional
+        maximum weight of any one single stock. The default is 0.4.
+    tol : float, optional
+        maximum difference between 1 and the projected weights total (want this to be as close to 0 as possible). The default is 1e-12.
+    max_iter : int, optional
+        Max number of bisection iterations. The default is 2000.
+
+    Raises
+    ------
+    ValueError
+        checks if the total is invalid
+
+    Returns
+    -------
+    W : array
+        Projected weights to be used in gradient descent
+
+    """
+    
+    #create vector n with length equal to the number of rows (same as the number of assets)
+    n = v.shape[0]
+    
+    #check if the total weights input is less than 0 or higher than the number of stocks * cap
+    if total < 0 or total > n * cap:
+        raise ValueError(f"Infeasible: total={total} not in [0, {n*cap}] for cap={cap}")
+
+    #generate bounds for bisection (min and max of tau)
+    lo = v.min() - cap
+    hi = v.max() 
+    
+    for _ in range(max_iter):
+        
+        #find tau by bisecting the lower bounds of tau 
+        tau = 0.5 * (lo + hi)
+        
+        #shift the weights by tau (so the sum equals "total") and project the shifted (weights - tau) between 0 and cap
+        w = np.clip(v - tau, 0.0, cap)
+        
+        #create a variable for the sum of all the weihgts
+        w_sum = w.sum()
+
+        #check if the sum adds to "total" within tolerance (if yes, return projected weights - if no, change lo/hi and recompute bisection to find tau so it does sum to "total")
+        if abs(w_sum - total) < tol:
+            return w
+        if w_sum > total:
+            lo = tau 
+        else:
+            hi = tau
+
+    return np.clip(v - 0.5*(lo+hi), 0.0, cap)
 
 
-
-
-def ridge_closed_form(cov_matrix, mu, lam):
-    p = len(mu)
-    Id = np.identity(p)
-    return np.linalg.solve(cov_matrix + lam * Id, mu)
-
+#function to calculate the objective function of ridge regression
 def ridge_objective(w, cov_matrix, mu, lam):
     return 0.5 * (w.T @ cov_matrix @ w) - (mu.T @ w) + 0.5 * lam * (w.T @ w)
 
+#function to calculate the gradient of ridge regression
 def ridge_grad(w, cov_matrix, mu, lam):
     return cov_matrix @ w - mu + lam * w
 
-#ridge regression using gradient descent
-def ridge_portfolio_gd(cov_matrix, mu, lam, step_size, tol=1e-6, max_iter=50000, cap=0.05):
+
+def ridge_portfolio_pgd(cov_matrix, mu, lam, step_size, cap=0.4, tol=1e-8, max_iter=20000):
     """
-    Ridge gradient descent with FIXED step size, then projection
-    to (constraints) (long-only, sum-to-1, max weight = cap).
+    Projected GD for ridge mean-variance objective with constraints:
+      sum(w)=1, 0<=w<=cap
+
+    Parameters
+    ----------
+    cov_matrix : matrix
+        Covariance matrix
+    mu : float
+        expected return.
+    lam : float
+        regularisation strength.
+    step_size : float
+        fixed step gradient descent stepsize
+
+    cap : float, optional
+        maximum weight that can be given to any single stock. The default is 0.4.
+    tol : float, optional
+        stopping critera for grdadient descent (if the distance between the new and old weights is sufficiently small this will trigger). The default is 1e-8.
+    max_iter : int, optional
+        maximum number of iterations of gradient descent (if tol isnt reached before this number of iteratiions is reached, this will stop the loop). The default is 20000.
+
+    Returns
+    -------
+    w : array
+        weights of each stock.
+    obj_vals : float
+        objective values when output weights are input into the original objective function.
     """
+ 
+    
+
+    #p: length of expected returns
     p = len(mu)
+    
+    #create a temporary weights vector of equal entries 
     w = np.ones(p) / p
+    
+    #empty array for objective values to be stored in
     obj_vals = []
 
-    for i in range(max_iter):
+    for _ in range(max_iter):
+        
+        #calculate descent direction (gradient)
         grad = ridge_grad(w, cov_matrix, mu, lam)
-
-        w_new = w - step_size * grad
-        w_new = apply_constraints(w_new, cap)
-
+        
+        #calculate next step w_new =  (w - t*grad(f(x)), then project on desired conditions
+        w_new = projection(w - step_size * grad, total = 1.0, cap = cap)
+        
+        #check if tolerance has been reached - if not repeat another iteration
         if np.linalg.norm(w_new - w) < tol:
             w = w_new
             break
-
+        
+        #change weights to the weights we just projected
         w = w_new
-        obj_vals.append(ridge_objective(w, cov_matrix, mu, lam))
+        
+        #append objective values to the array
+        obj_vals.append(0.5 * w.T @ cov_matrix @ w - mu.T @ w + 0.5 * lam * w.T @ w)
 
     return w, obj_vals
-
-
 
 
 # Parameters
 risk_free_rate = 0.04
 cap = 1
 threshold = 0.03
-lam = 5
+lam = 2
 step_size = 0.01
 
-#ridge regression
-#gradient descent approximation 
-# weights_ridge, obj_vals_ridge = ridge_portfolio_gd(cov_matrix, mu, lam, step_size, cap=cap)
+weights, obj_vals_ridge = ridge_portfolio_pgd(cov_matrix, mu, lam, step_size, cap=cap)
 
-
-#closed-form solution then apply constraints
-weights_ridge = apply_constraints(ridge_closed_form(cov_matrix, mu, lam), cap=cap)
-obj_vals_ridge = []
-
-
-#create a pandas series of tickers and corresponding weights
-weights_ridge_series = pd.Series(weights_ridge, index=tickers)
-
-#check if absolute of weights is greater than ridge and redistribute proportionally 
-weights_ridge_series = weights_ridge_series[weights_ridge_series.abs() > threshold]
-weights_ridge_series /= weights_ridge_series.sum()
-
-
-
+#create a pandas series of tickers and corresponding weights 
+weights_series = pd.Series(weights, index=tickers)
 
 #print the companies ticker and corresponding weights to 4dp
 print("\nRidge Portfolio Weights:\n")
-for t, w in weights_ridge_series.items():
+for t, w in weights_series.items():
     print(f"{t}: {w:.4f}")
 
 #calculate and print the key data as defined earlier
-ridge_return = expected_return(weights_ridge, log_returns)
-ridge_vol = standard_deviation(weights_ridge, cov_matrix)
-ridge_sharpe = sharpe_ratio(weights_ridge, log_returns, cov_matrix, risk_free_rate)
+ridge_return = expected_return(weights, log_returns)
+ridge_vol = standard_deviation(weights, cov_matrix)
+ridge_sharpe = sharpe_ratio(weights, log_returns, cov_matrix, risk_free_rate)
 
 print("\nRIDGE Portfolio Performance:")
 print(f"Expected Return: {ridge_return:.4f}")
@@ -147,7 +209,7 @@ print(f"Sharpe Ratio:    {ridge_sharpe:.4f}")
 
 #plot the weights on a bar chart
 plt.figure(figsize=(10, 6))
-plt.bar(weights_ridge_series.index, weights_ridge_series.values)
+plt.bar(weights_series.index, weights_series.values)
 plt.xticks(rotation=45)
 plt.ylabel("Weight")
 plt.title("Ridge Regression Portfolio Weights")
